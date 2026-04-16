05c772ddef4ef0202d81741ba1b62cdd8433e035
Md Kaif <mohammadkaif1204@gmail.com>
2026-04-16 23:12:10 +0530

    Fixed CORS for frontend

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 connectDB();
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173","https://mosque-donation-app-six.vercel.app"];
 
 app.use(express.json());
 app.use(cookieparser());
